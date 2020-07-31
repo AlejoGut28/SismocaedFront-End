b728298c1de03ebbba10f06fc79fdc2733f824a9
Alejandro <alejandrogutierrez@upeu.edu.pe>
2020-07-31 18:08:42 -0500

    subiendo ngx-pagination

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+
 import {NgxPaginationModule} from 'ngx-pagination';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
